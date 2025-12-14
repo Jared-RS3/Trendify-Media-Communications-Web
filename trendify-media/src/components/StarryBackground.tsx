5603e6117b2e152141dbd1d0c5e0156cd7e7b54f
DayBreak <daybreaktechinnovations@gmail.com>
2025-12-14 23:44:59 +0200

    Add Trendify Media website with starry background

---
+++ trendify-media/src/components/StarryBackground.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./StarryBackground.css";
+
+const StarryBackground: React.FC = () => {
+  return <div className="starry-bg" aria-hidden="true" />;
+};
+
+export default StarryBackground;
